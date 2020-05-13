265f68e7b4f101cb4aae08dc8d691a8da90ecdc3
akhila-29 <61230691+akhila-29@users.noreply.github.com>
2020-05-13 19:11:03 +0530

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# # IBM Data Science Professional Specialization
+#   
+# This notebook is for the capstone project for the 9-courses specialization in Data Science of IBM. Platform Coursera. Data Science Specialization.
+# 
+# ## B Akhila Reddy
+# 
+# E-mail:bakhilareddy29@gmail.com
+# LinkedIn:https://www.linkedin.com/in/akhila-reddy-978363169/
+# 
+# ## CHANGE LOG:
+# 
+# 1. May 13th, 2020: Initialize notebook
+# 
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
